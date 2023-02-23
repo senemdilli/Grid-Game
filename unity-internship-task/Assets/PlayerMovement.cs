bd5caae7f37ae6e272f77532ba57d075f672f037
Senem Dilli <senemdilli5@gmail.com>
2023-02-23 15:37:27 +0300

    Adding Tile Grid & Grid Base Movement

--- unity-internship-task/Assets/PlayerMovement.cs
+++ unity-internship-task/Assets/PlayerMovement.cs
@@ -5,18 +5,44 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public Transform movePoint;
+
+    public LayerMask whatStopsMovement;
 
     public Rigidbody2D rb;
     public Animator animator;
 
     Vector2 movement;
 
-    // Update is called once per frame
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
     void Update()
     {
         // Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.5f)
+        {
+            if (Mathf.Abs(movement.x) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(movement.x, 0f, 0f), .2f, whatStopsMovement))
+                {
+                    movePoint.position += new Vector3(movement.x, 0f, 0f);
+                }
+            } else if (Mathf.Abs(movement.y) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, movement.y, 0f), .2f, whatStopsMovement))
+                {
+                    movePoint.position += new Vector3(0f, movement.y, 0f);
+                }
+            }
+        }
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
